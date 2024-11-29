5fa6450ee42ff5f91895763e324271e2a8a93f58
Ali Mar NGOM <122835666+alimar440@users.noreply.github.com>
2024-11-29 12:08:56 +0000

    review

--- p006buycars/lib/view/login.view.dart
+++ p006buycars/lib/view/login.view.dart
@@ -12,24 +12,26 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        //  padding: EdgeInsets.all(20),
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  "https://images.unsplash.com/photo-1517949909155-9cd6e7f839c6"),
-              fit: BoxFit
-                  .cover, // Adjust the image's fit (e.g., cover, contain, etc.)
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Column(
+        padding: EdgeInsets.all(0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1731354233513-60e9edaddc5d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
                 children: [
                   CircleAvatar(
                     backgroundColor: Colors.deepPurple,
@@ -42,7 +44,7 @@ class _LoginViewState extends State<LoginView> {
                   ),
                   SizedBox(height: 20),
                   Text(
-                    "S'inscrire",
+                    "Se connecter",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 30,
@@ -51,88 +53,88 @@ class _LoginViewState extends State<LoginView> {
                   ),
                 ],
               ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    SizedBox(height: 20),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        label: Text("Email"),
-                        hintText: "Entrer votre email",
-                        prefixIcon: Icon(
-                          Icons.email,
-                          color: Colors.deepPurple,
-                        ),
-                        border: OutlineInputBorder(),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      label: Text("Email"),
+                      hintText: "Entrer votre email",
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.deepPurple,
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    obscureText: _obscureText,
+                    decoration: InputDecoration(
+                      label: Text("Mot de passe"),
+                      hintText: "Entrer votre Mot de passe",
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.deepPurple,
+                      ),
+                      border: OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                        icon: _obscureText
+                            ? Icon(Icons.visibility)
+                            : Icon(Icons.visibility_off),
                       ),
                     ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      obscureText: _obscureText,
-                      decoration: InputDecoration(
-                        label: Text("Mot de passe"),
-                        hintText: "Entrer votre Mot de passe",
-                        prefixIcon: Icon(
-                          Icons.lock,
-                          color: Colors.deepPurple,
-                        ),
-                        border: OutlineInputBorder(),
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _obscureText = !_obscureText;
-                            });
-                          },
-                          icon: _obscureText
-                              ? Icon(Icons.visibility)
-                              : Icon(Icons.visibility_off),
-                        ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Valider",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Valider",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.deepPurple,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 18, horizontal: 80),
-                        textStyle: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 18, horizontal: 100),
+                      textStyle:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "S'enregistrer",
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "S'enregistrer",
-                        style: TextStyle(
-                          color: Colors.deepPurple,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        padding:
-                            EdgeInsets.symmetric(vertical: 18, horizontal: 80),
-                        textStyle: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 18, horizontal: 80),
+                      textStyle:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
